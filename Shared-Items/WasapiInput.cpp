6bc5b8265515f6589a3e0723fb61fbe356cd6a5b
Allen Pestaluky <allen@avlatency.com>
2021-11-24 12:48:58 -0500

    Copied wasapi capture sample code from msdn

--- Shared-Items/WasapiInput.cpp
+++ Shared-Items/WasapiInput.cpp
@@ -23,6 +23,116 @@ WasapiInput::~WasapiInput()
 void WasapiInput::StartRecording()
 {
     recordingInProgress = true;
+
+    HRESULT hr;
+    REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_SEC;
+    REFERENCE_TIME hnsActualDuration;
+    UINT32 bufferFrameCount;
+    UINT32 numFramesAvailable;
+    IMMDeviceEnumerator* pEnumerator = NULL;
+    IMMDevice* pDevice = NULL;
+    IAudioClient* pAudioClient = NULL;
+    IAudioCaptureClient* pCaptureClient = NULL;
+    WAVEFORMATEX* pwfx = NULL;
+    UINT32 packetLength = 0;
+    BOOL bDone = FALSE;
+    BYTE* pData;
+    DWORD flags;
+
+    hr = CoCreateInstance(
+        CLSID_MMDeviceEnumerator, NULL,
+        CLSCTX_ALL, IID_IMMDeviceEnumerator,
+        (void**)&pEnumerator);
+    EXIT_ON_ERROR(hr)
+
+        hr = pEnumerator->GetDefaultAudioEndpoint(
+            eCapture, eConsole, &pDevice);
+    EXIT_ON_ERROR(hr)
+
+        hr = pDevice->Activate(
+            IID_IAudioClient, CLSCTX_ALL,
+            NULL, (void**)&pAudioClient);
+    EXIT_ON_ERROR(hr)
+
+        hr = pAudioClient->GetMixFormat(&pwfx);
+    EXIT_ON_ERROR(hr)
+
+        hr = pAudioClient->Initialize(
+            AUDCLNT_SHAREMODE_SHARED,
+            0,
+            hnsRequestedDuration,
+            0,
+            pwfx,
+            NULL);
+    EXIT_ON_ERROR(hr)
+
+        // Get the size of the allocated buffer.
+        hr = pAudioClient->GetBufferSize(&bufferFrameCount);
+    EXIT_ON_ERROR(hr)
+
+        hr = pAudioClient->GetService(
+            IID_IAudioCaptureClient,
+            (void**)&pCaptureClient);
+    EXIT_ON_ERROR(hr)
+
+        // Notify the audio sink which format to use.
+        hr = pMySink->SetFormat(pwfx);
+    EXIT_ON_ERROR(hr)
+
+        // Calculate the actual duration of the allocated buffer.
+        hnsActualDuration = (double)REFTIMES_PER_SEC *
+        bufferFrameCount / pwfx->nSamplesPerSec;
+
+    hr = pAudioClient->Start();  // Start recording.
+    EXIT_ON_ERROR(hr)
+
+        // Each loop fills about half of the shared buffer.
+        while (bDone == FALSE)
+        {
+            // Sleep for half the buffer duration.
+            Sleep(hnsActualDuration / REFTIMES_PER_MILLISEC / 2);
+
+            hr = pCaptureClient->GetNextPacketSize(&packetLength);
+            EXIT_ON_ERROR(hr)
+
+                while (packetLength != 0)
+                {
+                    // Get the available data in the shared buffer.
+                    hr = pCaptureClient->GetBuffer(
+                        &pData,
+                        &numFramesAvailable,
+                        &flags, NULL, NULL);
+                    EXIT_ON_ERROR(hr)
+
+                        if (flags & AUDCLNT_BUFFERFLAGS_SILENT)
+                        {
+                            pData = NULL;  // Tell CopyData to write silence.
+                        }
+
+                    // Copy the available capture data to the audio sink.
+                    hr = pMySink->CopyData(
+                        pData, numFramesAvailable, &bDone);
+                    EXIT_ON_ERROR(hr)
+
+                        hr = pCaptureClient->ReleaseBuffer(numFramesAvailable);
+                    EXIT_ON_ERROR(hr)
+
+                        hr = pCaptureClient->GetNextPacketSize(&packetLength);
+                    EXIT_ON_ERROR(hr)
+                }
+        }
+
+    hr = pAudioClient->Stop();  // Stop recording.
+    EXIT_ON_ERROR(hr)
+
+        Exit:
+    CoTaskMemFree(pwfx);
+    SAFE_RELEASE(pEnumerator)
+        SAFE_RELEASE(pDevice)
+        SAFE_RELEASE(pAudioClient)
+        SAFE_RELEASE(pCaptureClient)
+
+        return hr;
 }
 
 void WasapiInput::StopRecording()
