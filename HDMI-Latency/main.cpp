e33e2f10cea63cfe8e65ec70aa017aed492b4c82
Allen Pestaluky <allen@avlatency.com>
2021-12-13 15:18:35 -0500

    Renamed HDMI Audio Latency app window

--- HDMI-Latency/main.cpp
+++ HDMI-Latency/main.cpp
@@ -45,7 +45,7 @@ int main(int, char**)
 
     WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("AV Latency.com HDMI Latency"), NULL };
     ::RegisterClassEx(&wc);
-    HWND hwnd = ::CreateWindow(wc.lpszClassName, _T("AV Latency.com HDMI Latency"), WS_OVERLAPPEDWINDOW, r.left, r.top, windowWidth * Gui::DpiScale, windowHeight * Gui::DpiScale, NULL, NULL, wc.hInstance, NULL);
+    HWND hwnd = ::CreateWindow(wc.lpszClassName, _T("AV Latency.com HDMI Audio Latency"), WS_OVERLAPPEDWINDOW, r.left, r.top, windowWidth * Gui::DpiScale, windowHeight * Gui::DpiScale, NULL, NULL, wc.hInstance, NULL);
 
     // Initialize Direct3D
     if (!CreateDeviceD3D(hwnd))
