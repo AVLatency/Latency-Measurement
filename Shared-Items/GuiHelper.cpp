2a877d29baee325a2c3b5b142f1dceb9bfc68d0e
Allen Pestaluky <allen@avlatency.com>
2022-03-22 13:23:22 -0400

    Added a number of troubleshooting steps to the volume adjustment screen

--- Shared-Items/GuiHelper.cpp
+++ Shared-Items/GuiHelper.cpp
@@ -175,6 +175,8 @@ void GuiHelper::AdjustVolumeDisplay(const char* imGuiID, const AdjustVolumeManag
     }
 
     ImGui::Checkbox("Automatic Threshold Detection", useAutoThreshold);
+    ImGui::SameLine();
+    HelpMarker("Automatically sets the audio pattern detection threshold to half of the largest edge magnitude. Disable this feature to increase the threshold when there is loud cable crosstalk or background noise.");
     if (!*useAutoThreshold)
     {
         ImGui::DragFloat("Manual Threshold", manualThreshold, 0.001, 0, 1.9, "%.4f", ImGuiSliderFlags_AlwaysClamp | ImGuiSliderFlags_Logarithmic);
@@ -184,6 +186,8 @@ void GuiHelper::AdjustVolumeDisplay(const char* imGuiID, const AdjustVolumeManag
         ImGui::SameLine();
     }
     ImGui::Checkbox("Cable Crosstalk Detection", cableCrosstalkDetection);
+    ImGui::SameLine();
+    HelpMarker("Ensures accuracy of measurements by detecting when cable crosstalk may be exceeding the current threshold setting.");
 
     ImGui::PopID();
 }
@@ -221,7 +225,7 @@ void GuiHelper::AdjustVolumeInstructionsTroubleshooting(int lastCheckedInputSamp
     ImGui::Text("Basic Instructions:");
     ImGui::PopFont();
     ImGui::SameLine();
-    ImGui::Text("Adjust the output volume of the DUT and adjust your Windows input device volume level to make the Signal Quality for both channels OK.");
+    ImGui::Text("Adjust microphone placement, output volume of the DUT, and input device volume to make the Signal Quality for both channels OK.");
     ImGui::Spacing();
 
     if (ImGui::TreeNode("Example"))
@@ -233,27 +237,48 @@ void GuiHelper::AdjustVolumeInstructionsTroubleshooting(int lastCheckedInputSamp
     }
     if (ImGui::TreeNode("Detailed Instructions and Troubleshooting"))
     {
+        ImGui::PushFont(FontHelper::BoldFont);
+        ImGui::Text("Current Input Sample Rate");
+        ImGui::PopFont();
+        ImGui::Indent();
+            ImGui::Text(std::format("{} Hz", lastCheckedInputSampleRate).c_str());
+            ImGui::SameLine();
+            HelpMarker("An input device sample rate of 44100 Hz or higher is recommended. This sample rate can be configured in the Windows control panel.");
+        ImGui::Unindent();
+        
+        ImGui::Spacing();
+        ImGui::PushFont(FontHelper::BoldFont);
+        ImGui::Text("How to Ensure Cable Wiring is Correct");
+        ImGui::PopFont();
+        ImGui::Indent();
+            ImGui::TextWrapped("The level meters on the left side of the \"Raw Wave View\" show the current volume of each channel. "
+                "These can be used to give immediate feedback on the audio input of each channel, which can help in determining if your wiring is correct. "
+                "Simply tap on the microphone or unplug and re-plug a device and watch for a change in this meter. "
+                "If your wiring is correct, changes to one channel should not affect the other.");
+        ImGui::Unindent();
 
-        //    GuiHelper::PeakLevel(adjustVolumeManager->leftChannelGrade, "Adjust the volume of your input device through the Windows control panel to make the monitor amplitude fit with some headroom to spare. "
-        //        "You may need to turn down the Microphone Boost in the Levels section of Additional device properties.");
+        ImGui::Spacing();
+        ImGui::PushFont(FontHelper::BoldFont);
+        ImGui::Text("How to Fix \"Cable crosstalk detected\"");
+        ImGui::PopFont();
+        ImGui::Indent();
+        ImGui::TextWrapped("Cable crosstalk is most often detected when one of the two channels is recieving no audio signal at all. "
+            "When no audio signal is present, a very weak cable crosstalk signal is detected instead. "
+            "To address this, first make sure that your wiring is correct and that both channels are receiving audio input. "
+            "Next, try manually increasing the Threshold by disabling the \"Automatic threshold detection\" to bring the Threshold above the cable crosstalk signal. "
+            "Finally, if those two strategies do not resolve the issue, you may need to use an inline analog volume control on the line-level device to reduce its audio level and, in turn, reduce the cable crosstalk that it is causing.\n\n"
+            "NOTE: In cases that are very rare for digital audio, the audio signal for both channels may be very closely aligned. When this happens, you may need to disable cable crosstalk detection. "
+            "Before doing so, it is important to test a number of devices and become familiar with how this tool works and gain confidence in your wiring/microphone setup and audio levels.");
+        ImGui::Unindent();
 
-        //        GuiHelper::PeakLevel(adjustVolumeManager->rightChannelGrade, "Adjust the output volume of your Device Under Test (DUT) to give a consistent normalized recording.\n\n"
-        //            "When the DUT is muted, this peak level should be \"Quiet\". If it is not, this likely means you are getting cable crosstalk and your measurements will incorrectly be 0 ms audio latency!\n\n"
-        //            "To solve the problem of cable crosstalk, try turning down the output volume in the Advanced Configuration or using a physical, inline volume control on your HDMI Audio Extractor output.");
-
-        // Notes on determining if leftand right are wired the right way around
-        //      Maybe a small note about cable crosstalk
-        // Then notes on “if this is quiet / noisy”
-        //    turn up volume of DUT if quiet.Place mic close to speaker, etc
-        // consider adjusting these volumes instead : -DUT volume, -Input volume, -Distance from speaker to mic, -Use inline volume control on HDMI audio extractor
-
-        ImGui::Text(std::format("Input Sample Rate: {} Hz (recommended: 48000 Hz)", lastCheckedInputSampleRate).c_str());
-        if (lastCheckedInputSampleRate > 48000)
-        {
-            ImGui::Text(std::format("Higher sample rates may introduce high frequency noise.\n\n"
-                "A sample rate of48000 Hz may work better with this tool.\n\n"
-                "Use the Windows sound settings for your selected input device under\n\"Additional device properties\" to change the sample rate.", lastCheckedInputSampleRate).c_str());
-        }
+        ImGui::Spacing();
+        ImGui::PushFont(FontHelper::BoldFont);
+        ImGui::Text("How to Fix \"Noisy / Quiet\"");
+        ImGui::PopFont();
+        ImGui::Indent();
+            ImGui::TextWrapped("First ensure your cable wiring is correct using the steps above. Second, turn up the volume of the DUT and position the microphone closer to the DUT's left speaker to increase the signal to noise ratio. "
+                "You may also want to check your audio device volume / microphone boost settings.");
+        ImGui::Unindent();
 
         ImGui::TreePop();
     }
