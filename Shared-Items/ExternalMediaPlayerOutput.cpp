75db800627bf568726176bda0f3eb655a7d31e94
Allen Pestaluky <allen@avlatency.com>
2023-09-11 16:42:41 -0400

    Fixed typo in ExternalMediaPlayerOutput that caused the file name not to be set

--- Shared-Items/ExternalMediaPlayerOutput.cpp
+++ Shared-Items/ExternalMediaPlayerOutput.cpp
@@ -3,7 +3,7 @@
 //#include <shellapi.h>
 #include <format>
 
-ExternalMediaPlayerOutput::ExternalMediaPlayerOutput(std::string filePath, bool loop) : fileName(fileName), loop(loop)
+ExternalMediaPlayerOutput::ExternalMediaPlayerOutput(std::string fileName, bool loop) : fileName(fileName), loop(loop)
 {
 }
 
@@ -16,7 +16,8 @@ void ExternalMediaPlayerOutput::StartPlayback()
     do
     {
         std::string filePath = std::format("Audio Files\\{}", fileName);
-        system(std::format("\"\"C:\\Program Files (x86)\\K-Lite Codec Pack\\MPC-HC64\\mpc-hc64.exe\" \"{}\" /play /close \"", filePath).c_str());
+        std::string command = std::format("\"\"C:\\Program Files (x86)\\K-Lite Codec Pack\\MPC-HC64\\mpc-hc64.exe\" \"{}\" /play /close \"", filePath);
+        system(command.c_str());
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
         if (stopRequested.load(std::memory_order_acquire))
         {
